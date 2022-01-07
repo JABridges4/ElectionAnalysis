92c8939247d0415e2276dbff151efcd594546701
JABridges4 <95454820+JABridges4@users.noreply.github.com>
2022-01-07 14:14:04 -0600

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,28 @@
+# # Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+# Get the name of each candidate that recieved votes
+# Create a vote counter for each candidate
+# Get the total number of votes cast
+# Get the percentage of votes for each candidate
+# Declare a winner based on who got the most votes
